fd90863482118ccc43396d09557b0dd98e918b92
Malavika Ramakrishnan <malavika1506@gmail.com>
2020-03-09 19:53:53 +0100

    9/3

--- preprocessing/primary_actor.py
+++ preprocessing/primary_actor.py
@@ -1,19 +1,13 @@
 import pandas as pd
 import numpy as np 
 import ast
-import nltk
-from nltk.stem.snowball import SnowballStemmer
+import os
 import re
-from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.tokenize import RegexpTokenizer
-from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer
-from sklearn.cluster import KMeans
-from sklearn.metrics.pairwise import cosine_similarity
+import seaborn as sns
+import tensorflow as tf
+import tensorflow_hub as hub
 
-
-
-file_path=('C:/Users/malav/Desktop/dat500-project/output.txt')
+file_path=('C:/Users/malav/Desktop/dat500-project/output2.txt')
 
 actor=[]
 with open(file_path,'rt') as f:
@@ -24,28 +18,32 @@ with open(file_path,'rt') as f:
 #Generating ranked list of primary (actor[0]) actors and avg genre score: 
 actors=[]
 for n,ID in enumerate(list(actor[0].values())[0]):
-    for k,v in ID.items():
-        if v:
-            if len(v)==1:
-                score=(list(v[0].values()))
-                actors.append({k: score[0]})
-            else:
-                t=(len(v))
-                tot=[]
-                for i in range(0,t):
-                    tot.append((list(v[i].values()))[0])
-                score=(sum(tot)/t)
-                actors.append({k: score})
+    if ID:
+        for k,v in ID.items():
+            if v:
+                if len(v)==1:
+                    score=(list(v[0].values()))
+                    actors.append({k: score[0]})
+                else:
+                    t=(len(v))
+                    tot=[]
+                    for i in range(0,t):
+                        tot.append((list(v[i].values()))[0])
+                    score=(sum(tot)/t)
+                    actors.append({k: score})
 aid=[]
 g_score=[]
 for i in actors:
     aid.append(list(i.keys())[0])
     g_score.append(list(i.values())[0])
 
+
 #Ranked actor_ID     
 data0=(pd.DataFrame([aid,g_score], index=None).T)
 data0.columns=['Actor ID', 'Avg Genre Score']
 data0=data0.sort_values(by=['Avg Genre Score'], ascending= False)
+
+#print(data0)
 
 #Collecting titles from principal.tsv
 db=pd.read_csv('C:/Users/malav/Desktop/dat500-project/principals.tsv', sep='\t')
@@ -54,6 +52,8 @@ df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
 
 #list of titles per actor ID
 df1=df.groupby(['nconst'])['tconst'].apply(list)
+
+#print(df1)
 
 #Comparing plot 
 file_path1=('C:/Users/malav/Desktop/dat500-project/input.txt')
@@ -69,6 +69,8 @@ summ=pd.read_csv('C:/Users/malav/Desktop/dat500-project/summary_box_office.tsv',
 df2=pd.merge(df, summ, how='inner', left_on='tconst', right_on='tconst')
 df2=df2.drop(['nconst','boxoffice'],axis=1)
 
+#print(df2)
+
 summary=list(df2['summary'])
 
 #Setting all non-str values to 'NA'
@@ -79,52 +81,50 @@ for text in summary:
     else:
         summaries.append(text)
 
-# Define a function to perform both stemming and tokenization
-stemmer = SnowballStemmer("english")
-def tokenize_and_stem(text):
-    
-    # Tokenize by sentence, then by word
-    tokens = [word for sentence in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sentence)]
-    
-    # Filter out raw tokens to remove noise
-    filtered_tokens = [token for token in tokens if re.search('[a-zA-Z]', token)]
-    
-    # Stem the filtered_tokens
-    stems = [stemmer.stem(token) for token in filtered_tokens]
-    return stems
+#importing the pre-trained model 
+module_url = "https://tfhub.dev/google/universal-sentence-encoder/4" 
+model = hub.load(module_url)
+print ("module %s loaded" % module_url)
 
-# Instantiate TfidfVectorizer object with stopwords and tokenizer
-# parameters for efficient processing of text
-tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=200000,
-                                 min_df=0.2, stop_words='english',
-                                 use_idf=True, tokenizer=tokenize_and_stem,
-                                 ngram_range=(1,3))
+def embed(input):
+    return model(input)
 
-movies=summaries+list(plot)
-# Fit and transform the tfidf_vectorizer with the "plot" of each movie
-# to create a vector representation of the plot summaries
-tfidf_matrix = tfidf_vectorizer.fit_transform([x for x in movies])
+s=[plot]
+summaries=s+summaries
 
-#print(tfidf_matrix.shape)
+sims = embed(summaries)
+sims2 = []
+for i in range(len(summaries)):
+    sims2.append((i, np.dot(sims[0], sims[i])))
 
-# Create a KMeans object with 5 clusters and save as km
-km = KMeans(n_clusters=5)
+sims2.sort(key=lambda x: x[1], reverse=True)
 
-# Fit the k-means object with tfidf_matrix
-km.fit(tfidf_matrix)
-
-#corresponding cluster of movies in summaries 
-clusters = km.labels_.tolist()
-
-# Calculate the similarity distance
-similarity_distance = 1 - cosine_similarity(tfidf_matrix)
+#computing similar summaries to plot 
+summ_match=[]
+summ_score=[]
+sum_dic=[]
+for i in range(1,len(sims2)):
+   #print("{}. {}".format(sims2[i][1], summaries[sims2[i][0]]))
+   dic={(sims2[i][1]) : summaries[sims2[i][0]]}
+   #summ_score.append(sims2[i][1])
+   #summ_match.append(summaries[sims2[i][0]])
+   sum_dic.append(dic)
 
 
-def find_similar():
-  vector = similarity_distance[-1, :]
-  most_similar = movies[np.argsort(vector)[1]]
-  return most_similar
+summ_list=[]
+for i,text in enumerate(summary):
+    for summ in sum_dic:
+        boo=0
+        for key,value in summ.items():
+            if text== value:
+                summ_list.append(i)
+                summ_score.append(key)
+                boo=1
+                break
+        if boo==1:
+            break
 
-print(find_similar()) # prints "The Graduate"
-
-
+#print((summ_score))
+df_rank=df2[df2.index.isin(summ_list)]
+df_rank['summary_score']=summ_score
+print(df_rank)
